58094e496c513b9fd1022a1d04ca351fb164eead
sfmqur <sammaruska@gmail.com>
2017-01-23 16:12:03 -0600

    most of specs command, part of drag calculation, added a few engines

--- dragway.c
+++ dragway.c
@@ -10,16 +10,17 @@
 
 #define DEBUG 1
 #define bufferSize 50
+#define pi 3.14159265358979
 
 //structures
 struct Engine {
 	char name[bufferSize];
-	int hp; //horsepower
-	int redline;
+	double hp; //horsepower
+	double redline;
 };
 struct Transmission {
 	char name[bufferSize];
-	int numGears;
+	double numGears;
 	double ratios[6]; //6 speed transmissions max
 };
 struct Differential {
@@ -42,46 +43,99 @@ void quit(int *done) {
 	}
 }
 
-void drag() {
-	printf("You are now racing!!!\n");
-}
+//TODO: opponents time rename this function to return a double(time) and create new racing function.
+double dragCalc(struct Engine engine, struct Transmission trans,
+		struct Differential diff, struct Tire tire) {
+	double time = 0.0;
+	double myTime = 0;
+	double distance = 5280 / 4; //quarter mile drag
+	double myDistance = 0;
+	double mySpeed = 0;
+	double rpm = 0;
+	double rpmPerSecond = 5.156271577; //RPM per second per horsepower, default vehicle 0-60 is 7.9 seconds(Chevy Trailblazer)
+	double drdt = rpmPerSecond * engine.hp;
+	double tempdrdt = 0; //used when drdt + rpm exceeds redline
+	double tempstep = 0; //used when drdt + rpm exceeds redline
+	int gear = 1;
+	double step = 0.1;
 
-void specs() {
-	printf("You have some nice specs there\n");
-}
-
-void test(struct Engine *engines, int numEngines) {
-	int i;
-	for (i = 0; i < numEngines; i++) {
-		if (engines[i].name[0] != '\0') {
-			printf("%s\n", engines[i].name);
+	while (myDistance < distance) {
+		if (gear != trans.numGears && rpm != engine.redline) { //check to make sure engine and trans are not maxed out
+			if (rpm + (drdt * step) > engine.redline) { //does little time step
+				tempdrdt = engine.redline - rpm;
+				rpm += tempdrdt;
+				time += 1 / (rpmPerSecond * engine.hp / tempdrdt);
+				mySpeed = rpm / trans.ratios[gear - 1] / diff.ratio * pi
+						* tire.size * 60 / 12; //feet per second
+				myDistance += mySpeed * step;
+				if (gear != trans.numGears) { //switches gears if not in max gear
+					gear++;
+				}
+				continue;
+			}
+			time += step;
+			rpm = drdt * time; //eliminates roundoff error
+			mySpeed = rpm / trans.ratios[gear - 1] / diff.ratio * pi * tire.size
+					* 60 / 12; //feet per second
+			myDistance += mySpeed * step; // TODO: may exceed quarter mile
 		} else {
-			printf("Engine %d does not have a name\n", i);
+			myDistance += mySpeed * step;
 		}
+	}
+	return mySpeed;
+}
+
+void drag(struct Engine engine, struct Transmission trans,
+		struct Differential diff, struct Tire tire) {
+	double myTime = dragCalc(engine, trans, diff, tire);
+	printf("My time is %g seconds\n", myTime);
+}
+
+void specs(struct Engine engine, struct Transmission trans,
+		struct Differential diff, struct Tire tire) {
+	int i;
+
+	printf("Engine: %s %g horsepower %g RPM redline\n", engine.name, engine.hp,
+			engine.redline);
+	printf("Transmission: %s, %d gears ",trans.name,trans.numGears);
+	for (i = 0; i < trans.numGears; i++){
+		printf("%g ",trans.ratios[i]);
+		if (i == trans.numGears - 1){
+			printf("\n");
+		}
+	}
+	printf("Differential: %g\n",diff.ratio);
+	printf("Tires: %g inches\n",tire.size);
+}
+
+void test(struct Transmission trans) {
+	int i;
+	for (i = 0; i < trans.numGears; i++) {
+		printf("%g ",trans.ratios[i]);
 	}
 }
 
 int main() {
-	//variables
+//variables
 	int done = 0;
 	char choice[bufferSize];
 	int i = 0;
 	FILE *fp;
 	int switcher = 0; //used to alternate reading strings and numbers from files each line
 
-	//parts arrays
+//parts arrays
 	struct Engine engines[bufferSize];
 	struct Transmission trannies[bufferSize];
 	struct Differential diffs[bufferSize];
 	struct Tire tires[bufferSize];
 
-	int numEngines = 2; // could replace with end of file line detection
+	int numEngines = 4; // could replace with end of file line detection
 	int numTrans = 7;	//and increment these variables....this works fine
 	int numDiffs = 3;
 	int numTires = 2;
 
-	//import parts data from files
-	//import engine data
+//import parts data from files
+//import engine data
 	fp = fopen("engines.txt", "r");
 	if (fp == NULL) { //ends program if there is a file reading error
 		printf("Error from reading from file: engines.txt");
@@ -89,11 +143,11 @@ int main() {
 	for (i = 0; i < numEngines; i++) {
 		fgets(engines[i].name, bufferSize, fp);
 		engines[i].name[strlen(engines[i].name) - 1] = '\0'; //removes newline
-		fscanf(fp, "%d %d\n", &engines[i].hp, &engines[i].redline);
+		fscanf(fp, "%lf %lf\n", &engines[i].hp, &engines[i].redline);
 	}
 	fclose(fp);
 
-	//import transmission data
+//import transmission data
 	fp = fopen("transmissions.txt", "r");
 	if (fp == NULL) { //ends program if there is a file reading error
 		printf("Error from reading from file: transmissions.txt");
@@ -101,14 +155,14 @@ int main() {
 	for (i = 0; i < numTrans; i++) {
 		fgets(trannies[i].name, bufferSize, fp);
 		trannies[i].name[strlen(trannies[i].name) - 1] = '\0'; //removes newline
-		fscanf(fp, "%d %g %g %g %g %g %g\n", &trannies[i].numGears,
+		fscanf(fp, "%d %lf %lf %lf %lf %lf %lf\n", &trannies[i].numGears,
 				&trannies[i].ratios[0], &trannies[i].ratios[1],
 				&trannies[i].ratios[2], &trannies[i].ratios[3],
 				&trannies[i].ratios[4], &trannies[i].ratios[5]);
 	}
 	fclose(fp);
 
-	//import diff data
+//import diff data
 	fp = fopen("differentials.txt", "r");
 	if (fp == NULL) { //ends program if there is a file reading error
 		printf("Error from reading from file: differentials.txt");
@@ -116,11 +170,11 @@ int main() {
 	for (i = 0; i < numDiffs; i++) {
 		fgets(diffs[i].name, bufferSize, fp);
 		diffs[i].name[strlen(diffs[i].name) - 1] = '\0'; //removes newline
-		fscanf(fp, "%g\n", &diffs[i].ratio);
+		fscanf(fp, "%lf\n", &diffs[i].ratio);
 	}
 	fclose(fp);
 
-	//import tire data
+//import tire data
 	fp = fopen("tires.txt", "r");
 	if (fp == NULL) { //ends program if there is a file reading error
 		printf("Error from reading from file: tires.txt");
@@ -128,11 +182,15 @@ int main() {
 	for (i = 0; i < numTires; i++) {
 		fgets(tires[i].name, bufferSize, fp);
 		tires[i].name[strlen(tires[i].name) - 1] = '\0'; //removes newline
-		fscanf(fp, "%g\n", &tires[i].size);
+		fscanf(fp, "%lf\n", &tires[i].size);
 	}
 	fclose(fp);
 
-	//assign default parts
+//assign default parts
+	struct Engine myEngine = engines[0];
+	struct Transmission myTrans = trannies[0];
+	struct Differential myDiff = diffs[0];
+	struct Tire myTire = tires[0];
 
 	while (!done) { //game loop
 		printf("What would you like to do?\n");
@@ -144,11 +202,11 @@ int main() {
 		} else if (!strcmp(choice, "quit")) {
 			quit(&done);
 		} else if (!strcmp(choice, "drag")) {
-			drag();
+			drag(myEngine, myTrans, myDiff, myTire);
 		} else if (!strcmp(choice, "specs")) {
-			specs();
+			specs(myEngine, myTrans, myDiff, myTire);
 		} else if (!strcmp(choice, "test") && DEBUG) {
-			test(engines, numEngines);
+			test(myTrans);
 		} else {
 			printf(
 					"Please enter a valid choice.\n Type help for your options\n");
